99a27aa77b624fd0159f2561da149342420d975e
Nick B <nbourrereg@hotmail.com>
2024-11-18 11:28:33 -0500

    Renamed folder and added instructions

--- godot-EnvLoader/config/env_loader.gd
+++ godot-EnvLoader/config/env_loader.gd
@@ -1,3 +1,14 @@
+#region Instructions
+	#1. Copy the config folder into your project.
+	#2. Autoload env_loader.gd with class name as EnvLoader
+	#3. Rename env.example.txt to env.txt
+	#4. Update env.txt to suit your needs
+	#5. Add env.txt to .gitignore
+	
+	# Usage example:
+	# var api_key = EnvLoader.get_env("API_KEY")
+#endregion
+
 extends Node
 
 var env_data: Dictionary = {}
@@ -18,8 +29,10 @@ func load_env(file_path: String = "res://config/env.txt"):
 			if parts.size() == 2:
 				env_data[parts[0]] = parts[1]
 	else:
-		print("Environment file not found: ", file_path)
+		push_error("Environment file not found: ", file_path)
 
 # Retrieve an environment variable
 func get_env(key: String, default_value: String = "") -> String:
+	if not env_data.has(key):
+		push_warning("Requested environment variable not found: " + key)
 	return env_data.get(key, default_value)
